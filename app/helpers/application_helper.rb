ed034a441eb0e3120d1320898ac6bef4c55b8be4
Deimyts <camjonesdesign@gmail.com>
2014-01-18 12:33:42 -0600

    added static pages with dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#returning the page title based on page
+	def full_title(page_title)
+		base_title = "Codename Carnegie"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home page" do 
 
-  	it "should have the content 'Dale'" do 
-  		visit  '/static_pages/home'
-  		expect(page).to have_content('Sample App')
-    end
-  end
+	subject { page }
+
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
+
+  	describe "Home page" do 
+  		before { visit '/static_pages/home' }
+  		let(:heading) { 'Carnegie'}
+  		let(:page_title) { '' }
+
+	 	it_should_behave_like "all static pages"
+	 	it { should_not have_title(' | Home') }
+	 end
+
+  	describe "About page:" do
+  		before { visit '/static_pages/about' }
+  		let(:heading) { 'About'}
+  		let(:page_title) { 'About Us' }
+
+	  	it_should_behave_like "all static pages"
+	end
 end
+
