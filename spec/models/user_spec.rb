50ac7fc6312f3821b241a8f98ddd7b1e4d044f61
Deimyts <camjonesdesign@gmail.com>
2014-01-24 18:35:28 -0600

    finished signin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe User do
 	before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
+
 	subject { @user }
 
 	it { should respond_to(:name) }
@@ -10,6 +11,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
@@ -95,5 +97,9 @@ describe User do
 		end
 	end
 
-
+	#remember token
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+	end
 end
